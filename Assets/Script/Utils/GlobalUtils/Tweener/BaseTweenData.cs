1fa6935fae391e2c9f0294c7c9e92c59eb81914b
Karpys <remi.vedrenne.pro@gmail.com>
2023-08-25 01:02:47 +0200

    [CODE] Initial commit and add global utils

---
+++ Assets/Script/Utils/GlobalUtils/Tweener/BaseTweenData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public struct BaseTweenData
+{
+    public Transform TargetTransform;
+    public Vector3 EndValue;
+    public float Duration;
+}
---
+++ Assets/Script/Utils/GlobalUtils/Tweener/TweenerTypeData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace TweenCustom
+{
+
+    [Serializable]
+    public enum Ease
+    {
+        LINEAR,
+        EASE_IN_SIN,
+        EASE_OUT_SIN,
+        EASE_OUT_ELASTIC,
+    }
+
+    public enum TweenMode
+    {
+        NORMAL,
+        ADDITIVE,
+    }
+
+}
