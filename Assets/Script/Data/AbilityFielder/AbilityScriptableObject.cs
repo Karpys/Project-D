1002402c9fae23e3f7b2e43882ce28b16b277325
Karpys <remi.vedrenne.pro@gmail.com>
2024-01-09 02:19:19 +0100

    [EDITOR] Auto fielder editor property drawer

---
+++ Assets/Script/Data/AbilityFielder/AbilityScriptableObject.cs
@@ -0,0 +1,15 @@
+﻿using KarpysDev.KarpysUtils.AutoFielder;
+using UnityEngine;
+
+namespace Script.Data.AbilityFielder
+{
+    [CreateAssetMenu(fileName = "Ability", menuName = "Data/Ability", order = 0)]
+    public class AbilityScriptableObject : ScriptableObject,IFielder
+    {
+        [SerializeField] protected Fielder m_AbilityField = null;
+        [SerializeField] protected int m_MyNewField = 0;
+        [SerializeField] protected int m_MyNewField2= 0;
+        public Fielder Fielder => m_AbilityField;
+        public Object TargetObject => this;
+    }
+}
